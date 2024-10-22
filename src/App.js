6e4d5927f1a9f5b5f215db601c485b3b81d4b6ea
Max V Olofsson <max.olofsson@se.experis.com>
2024-10-22 11:05:39 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const baseURL = "https://max-olofsson-backend-app.azurewebsites.net/";
+
+  async function get(endpoint) {
+    const res = await fetch(baseURL + endpoint);
+    const data = await res.json();
+    console.log("Inside get " + baseURL + endpoint);
+    console.log("res:");
+    console.log(res);
+    return data;
+  }
+
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(
+    () =>
+      async function () {
+        const res = await get("authors");
+        console.log(res);
+        setAuthors(res.data);
+      },
+    []
+  );
+
+  console.log(authors)
+
   const value = "World";
-  return <div>Hello {value} v3</div>;
+  return <div>Hello {value} v2</div>;
 }
 
 export default App;
